3d4e28f943cc447ca2f3c108b25cb9338238fe14
Jeffthearcher <kimanigeoffrey9275@gmail.com>
2023-11-24 18:28:28 +0300

    Update Java script.js

--- Java script.js
+++ Java script.js
@@ -3,6 +3,12 @@
 
 /*
 GROUP MEMBERS;
+SCT211-0061/2022 - Joseph Amuyunzu;
+SCT211-0023/2022 - Teddy Muli;
+SCT211-0022/2022 - Adrian Baraka;
+SCT211-0869/2022 - Jacob Kasunzu John;
+SCT211-0032/2022 - Benedict Waweru;
+SCT211-0581/2021- Serena Mumbi Kimiri;
  
 */
 <script>
